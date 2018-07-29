078ee4062fe6a5c35d82822424b9ab7b62b3c8ac
aruncnt <aruncnt@gmail.com>
2018-07-29 21:51:06 +0530

    404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.plane')
+@section('body')
+
+<div class="account-pages"></div>
+<div class="clearfix"></div>
+
+<div class="wrapper-page">
+    <div class="ex-page-content text-center">
+        <div class="text-error"><span class="text-primary">4</span><i class="ti-face-sad text-pink"></i><span class="text-info">4</span></div>
+        <h2 class="text-white">Who0ps! Page not found</h2><br>
+        <p class="text-muted">This page cannot found or is missing.</p>
+        <br>
+        <a class="btn btn-success waves-effect waves-light" href="/"> Back to Home</a>
+
+    </div>
+</div>
+
+@stop
+
