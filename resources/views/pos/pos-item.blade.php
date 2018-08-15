97a8e2800fafc523bc58d1a4d181aaaa23330c07
avinashanand124 <avinashanand124>
2018-08-15 10:56:19 +0530

    POS

---
+++ resources/views/pos/pos-item.blade.php
@@ -0,0 +1,189 @@
+<div class="cat_description">
+    <div class="catname-lists scrtabs-allow-scrollbar">
+        <div class="scrtabs-tab-container" style="visibility: visible;"><div class="scrtabs-tab-scroll-arrow scrtabs-tab-scroll-arrow-left" style="display: block;"><span class="glyphicon glyphicon-chevron-left"></span></div><div class="scrtabs-tabs-fixed-container" style="width: 634.2px;"><div class="scrtabs-tabs-movable-container" style="width: 930px;"><ul class="nav nav-tabs">
+            <li class="gradient1"><a href="#">Category Name1</a></li>
+            <li class="gradient2"><a href="#">Category Name2</a></li>
+            <li class="gradient3"><a href="#">Category Name3</a></li>
+            <li class="gradient1"><a href="#">Category Name4</a></li>
+            <li class="gradient2"><a href="#">Category Name5</a></li>
+            <li class="gradient3"><a href="#">Category Name6</a></li>
+        </ul></div></div><div class="scrtabs-tab-scroll-arrow scrtabs-tab-scroll-arrow-right" style="display: block;"><span class="glyphicon glyphicon-chevron-right"></span></div></div>
+    </div>
+    <div class="tab-content">
+        <div class="boxes active gradient1 mCustomScrollbar _mCS_1"><div id="mCSB_1" class="mCustomScrollBox mCS-light mCSB_horizontal mCSB_inside" style="max-height: none;" tabindex="0"><div id="mCSB_1_container" class="mCSB_container mCS_x_hidden mCS_no_scrollbar_x" style="position: relative; top: 0px; left: 0px; width: 100%;" dir="ltr">
+            <div class="wrapper">
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+            </div>
+        </div><div id="mCSB_1_scrollbar_horizontal" class="mCSB_scrollTools mCSB_1_scrollbar mCS-light mCSB_scrollTools_horizontal" style="display: none;"><a href="#" class="mCSB_buttonLeft" oncontextmenu="return false;"></a><div class="mCSB_draggerContainer" style=""><div id="mCSB_1_dragger_horizontal" class="mCSB_dragger" style="position: absolute; min-width: 30px; width: 0px; left: 0px;" oncontextmenu="return false;"><div class="mCSB_dragger_bar"></div></div><div class="mCSB_draggerRail"></div></div><a href="#" class="mCSB_buttonRight" oncontextmenu="return false;"></a></div></div></div>
+        <div class="boxes gradient2 mCustomScrollbar _mCS_2" style=""><div id="mCSB_2" class="mCustomScrollBox mCS-light mCSB_horizontal mCSB_inside" style="max-height: 249px;" tabindex="0"><div id="mCSB_2_container" class="mCSB_container" style="position: relative; top: 0px; left: 0px; width: 100%;" dir="ltr">
+            <div class="wrapper">
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+            </div>
+        </div><div id="mCSB_2_scrollbar_horizontal" class="mCSB_scrollTools mCSB_2_scrollbar mCS-light mCSB_scrollTools_horizontal" style="display: block;"><a href="#" class="mCSB_buttonLeft" oncontextmenu="return false;" style="display: block;"></a><div class="mCSB_draggerContainer" style=""><div id="mCSB_2_dragger_horizontal" class="mCSB_dragger" style="position: absolute; min-width: 30px; display: block; width: 0px; left: 0px;" oncontextmenu="return false;"><div class="mCSB_dragger_bar"></div></div><div class="mCSB_draggerRail"></div></div><a href="#" class="mCSB_buttonRight" oncontextmenu="return false;" style="display: block;"></a></div></div></div>
+        <div class="boxes gradient3 mCustomScrollbar _mCS_3" style=""><div id="mCSB_3" class="mCustomScrollBox mCS-light mCSB_horizontal mCSB_inside" style="max-height: 249px;" tabindex="0"><div id="mCSB_3_container" class="mCSB_container" style="position: relative; top: 0px; left: 0px; width: 100%;" dir="ltr">
+            <div class="wrapper">
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+            </div>
+        </div><div id="mCSB_3_scrollbar_horizontal" class="mCSB_scrollTools mCSB_3_scrollbar mCS-light mCSB_scrollTools_horizontal" style="display: block;"><a href="#" class="mCSB_buttonLeft" oncontextmenu="return false;" style="display: block;"></a><div class="mCSB_draggerContainer" style=""><div id="mCSB_3_dragger_horizontal" class="mCSB_dragger" style="position: absolute; min-width: 30px; display: block; width: 0px; left: 0px;" oncontextmenu="return false;"><div class="mCSB_dragger_bar"></div></div><div class="mCSB_draggerRail"></div></div><a href="#" class="mCSB_buttonRight" oncontextmenu="return false;" style="display: block;"></a></div></div></div>
+        <div class="boxes gradient1 mCustomScrollbar _mCS_4" style=""><div id="mCSB_4" class="mCustomScrollBox mCS-light mCSB_horizontal mCSB_inside" style="max-height: 249px;" tabindex="0"><div id="mCSB_4_container" class="mCSB_container" style="position: relative; top: 0px; left: 0px; width: 100%;" dir="ltr">
+            <div class="wrapper">
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+            </div>
+        </div><div id="mCSB_4_scrollbar_horizontal" class="mCSB_scrollTools mCSB_4_scrollbar mCS-light mCSB_scrollTools_horizontal" style="display: block;"><a href="#" class="mCSB_buttonLeft" oncontextmenu="return false;" style="display: block;"></a><div class="mCSB_draggerContainer" style=""><div id="mCSB_4_dragger_horizontal" class="mCSB_dragger" style="position: absolute; min-width: 30px; display: block; width: 0px; left: 0px;" oncontextmenu="return false;"><div class="mCSB_dragger_bar"></div></div><div class="mCSB_draggerRail"></div></div><a href="#" class="mCSB_buttonRight" oncontextmenu="return false;" style="display: block;"></a></div></div></div>
+        <div class="boxes gradient2 mCustomScrollbar _mCS_5" style=""><div id="mCSB_5" class="mCustomScrollBox mCS-light mCSB_horizontal mCSB_inside" style="max-height: 249px;" tabindex="0"><div id="mCSB_5_container" class="mCSB_container" style="position: relative; top: 0px; left: 0px; width: 100%;" dir="ltr">
+            <div class="wrapper">
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+            </div>
+        </div><div id="mCSB_5_scrollbar_horizontal" class="mCSB_scrollTools mCSB_5_scrollbar mCS-light mCSB_scrollTools_horizontal" style="display: block;"><a href="#" class="mCSB_buttonLeft" oncontextmenu="return false;" style="display: block;"></a><div class="mCSB_draggerContainer" style=""><div id="mCSB_5_dragger_horizontal" class="mCSB_dragger" style="position: absolute; min-width: 30px; display: block; width: 0px; left: 0px;" oncontextmenu="return false;"><div class="mCSB_dragger_bar"></div></div><div class="mCSB_draggerRail"></div></div><a href="#" class="mCSB_buttonRight" oncontextmenu="return false;" style="display: block;"></a></div></div></div>
+        <div class="boxes gradient3 mCustomScrollbar _mCS_6" style=""><div id="mCSB_6" class="mCustomScrollBox mCS-light mCSB_horizontal mCSB_inside" style="max-height: 249px;" tabindex="0"><div id="mCSB_6_container" class="mCSB_container" style="position: relative; top: 0px; left: 0px; width: 100%;" dir="ltr">
+            <div class="wrapper">
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+                <div class="common"><a href="#">Item Name 1 <br> 2 lines</a></div>
+            </div>
+        </div><div id="mCSB_6_scrollbar_horizontal" class="mCSB_scrollTools mCSB_6_scrollbar mCS-light mCSB_scrollTools_horizontal" style="display: block;"><a href="#" class="mCSB_buttonLeft" oncontextmenu="return false;" style="display: block;"></a><div class="mCSB_draggerContainer" style=""><div id="mCSB_6_dragger_horizontal" class="mCSB_dragger" style="position: absolute; min-width: 30px; display: block; width: 0px; left: 0px;" oncontextmenu="return false;"><div class="mCSB_dragger_bar"></div></div><div class="mCSB_draggerRail"></div></div><a href="#" class="mCSB_buttonRight" oncontextmenu="return false;" style="display: block;"></a></div></div></div>
+        
+    </div>
+<!--
+    <div class="cat-pagination">
+        <a href="#"><i class="fa fa-angle-left"></i></a>
+        <a href="#"><i class="fa fa-angle-right"></i></a>
+    </div>
+-->
+</div>
