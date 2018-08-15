93ceee0f4d42c7cf4909752106101892accec3af
aruncnt <aruncnt@gmail.com>
2018-08-16 04:00:41 +0530

    tab bar added

--- resources/views/pos/brand.blade.php
+++ resources/views/pos/brand.blade.php
@@ -1,12 +1,12 @@
 <div class="categories_list">
 	<ul>
-		<li class="active"><a href="#"><img src="assets/images/brand-bunfire.png"></a></li>
-		<li><a href="#"><img src="assets/images/brand-jacks.png"></a></li>
-		<li><a href="#"><img src="assets/images/brand-nkd.png"></a></li>
-		<li><a href="#"><img src="assets/images/brand-right-bite.png"></a></li>
-		<li><a href="#"><img src="assets/images/brand-tawook.png"></a></li>
-		<li><a href="#"><img src="assets/images/brand-vigilante.png"></a></li>
-		<li><a href="#"><img src="assets/images/brand-right-bite.png"></a></li>
-		<li><a href="#"><img src="assets/images/brand-tawook.png"></a></li>
+		<li class="active"><a href="#"><img src="assets/images/order/brand-bunfire.png"></a></li>
+		<li><a href="#"><img src="assets/images/order/brand-jacks.png"></a></li>
+		<li><a href="#"><img src="assets/images/order/brand-nkd.png"></a></li>
+		<li><a href="#"><img src="assets/images/order/brand-right-bite.png"></a></li>
+		<li><a href="#"><img src="assets/images/order/brand-tawook.png"></a></li>
+		<li><a href="#"><img src="assets/images/order/brand-vigilante.png"></a></li>
+		<li><a href="#"><img src="assets/images/order/brand-right-bite.png"></a></li>
+		<li><a href="#"><img src="assets/images/order/brand-tawook.png"></a></li>
 	</ul>
 </div>
